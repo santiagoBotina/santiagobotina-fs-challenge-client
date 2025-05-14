6f7e6b418e9accfc69b9e6b1a9e543d4efd3d99a
santiagoBotina <santiagoboe04@gmail.com>
2025-05-14 08:49:46 -0500

    feat: add error messages constant

--- src/utils/app.constants.ts
+++ src/utils/app.constants.ts
@@ -9,3 +9,10 @@ export const AVAILABLE_VIEWS: VIEW[] = [
     {path: '/', label:'Shorten'},
     {path: '/top', label:'Top 100 URLs'}
 ];
+
+export const ErrorMessages = {
+    INVALID_URL: 'Invalid URL',
+    EMPTY_URL: 'URL cannot be empty',
+    SHORT_CODE_CREATION_FAILED: 'Failed to create short code',
+    SHORT_CODE_API_ERROR: 'Short code API error',
+}
