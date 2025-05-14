11274fba54c151eced75ff868f5afa8f956c521e
santiagoBotina <santiagoboe04@gmail.com>
2025-05-14 10:37:22 -0500

    chore: config test to read aliases

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,13 @@ export default {
     transform: {
         "^.+\\.tsx?$": "ts-jest",
     },
+    moduleNameMapper: {
+        "^@utils/(.*)$": "<rootDir>/src/utils/$1",
+        "^@adapters/(.*)$": "<rootDir>/src/core/adapters/$1",
+        "^@components/(.*)$": "<rootDir>/src/components/$1",
+        "^@pages/(.*)$": "<rootDir>/src/pages/$1",
+        "^@hooks/(.*)$": "<rootDir>/src/hooks/$1",
+        "^@domain/(.*)$": "<rootDir>/src/core/domain/$1",
+    },
     setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
