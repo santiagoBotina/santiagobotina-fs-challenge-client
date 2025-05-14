f174a76faf154a193e84b2c48752493e63ceec94
santiagoBotina <santiagoboe04@gmail.com>
2025-05-13 23:50:21 -0500

    chore: add router and remove unnecessary files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,40 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import {Home} from "@pages/home";
+import {Top100} from "@pages/top100";
+import {AVAILABLE_VIEWS} from "@utils/app.constants.ts";
+import {SideBar} from "@components/sidebar";
+
+const Layout = () => (
+    <div className="min-h-screen min-w-screen flex">
+        <SideBar viewsAvailable={AVAILABLE_VIEWS} />
+        <div className="flex-1 w-full">
+            <Outlet />
+        </div>
+    </div>
+);
+
+const router = createBrowserRouter([
+    {
+        element: <Layout />,
+        children: [
+            {
+                path: '/',
+                element: <Home />,
+            },
+            {
+                path: 'top',
+                element: <Top100 />,
+            },
+        ],
+        errorElement: <div>404 Not Found</div>,
+    }
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@domain": path.resolve(__dirname, "./src/core/domain"),
+      "@adapters": path.resolve(__dirname, "./src/core/adapters"),
+    },
+  }
 })
